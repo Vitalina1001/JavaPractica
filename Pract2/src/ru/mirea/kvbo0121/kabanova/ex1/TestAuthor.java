1349c718a91083354e5e53b972b40ed12b5e40b3
Vitalina1001 <101709483+Vitalina1001@users.noreply.github.com>
2022-09-19 19:35:03 +0300

    test

---
+++ Pract2/src/ru/mirea/kvbo0121/kabanova/ex1/TestAuthor.java
@@ -0,0 +1,9 @@
+package ru.mirea.kvbo0121.kabanova.ex1;
+
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author a = new Author("Иван", "ivan@ivan.ru", 'm');
+        System.out.println(a);
+    }
+}
